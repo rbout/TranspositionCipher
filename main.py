9de81d774f0980b171a50caa0200bd27c5c45401
Robert Boutillier <36680316+rbout@users.noreply.github.com>
2018-09-20 11:09:43 -0400

    Encrypt and decrypt functions

--- main.py
+++ main.py
@@ -1,63 +1,78 @@
 import math
 
+def encrypt(str, key):
+    # Encryption
+    keyLen = len(key)
+    padLen = len(s) % keyLen
+
+    # pads the message s so with spaces so that it can be encrypted
+    if padLen > 0:
+        for i in range(0, keyLen - padLen):
+            str = str + " "
+
+    sList = []
+    ind = 0
+    while ind < len(s):
+        sList.append(list(str[ind: ind + keyLen]))
+        ind = ind + keyLen
+    keyList = list(key)
+    keyList.sort()
+
+    newS = ""
+    for i in range(0, len(keyList)):
+        for x in range(0, len(sList)):
+            newS = newS + sList[x][keyList.index(key[i: i + 1])]
+
+    return newS
+
+
+def decrypt(str, key):
+    # Decryption
+    keyLen = len(key)
+    keyList = list(key)
+
+    outlist = []
+    for i in range(0, keyLen):
+        outlist.append([])
+
+    numrows = math.ceil(len(str) / keyLen)
+
+    fullcols = len(s) % keyLen
+    if fullcols == 0:
+        fullcols = keyLen
+
+    lenslice = 0
+    begslice = 0
+
+    for index in range(0, keyLen):
+        keyletter = key[index]
+        keyindex = keyList.index(keyletter)
+
+        if keyindex >= fullcols:
+            lenslice = numrows - 1
+        else:
+            lenslice = numrows
+
+        colslice = str[begslice: begslice + lenslice]
+        begslice += lenslice
+
+        outlist[keyindex] = list(colslice)
+
+    outstr = ""
+
+    for index in range(0, len(str)):
+        row = index // keyLen
+        col = index % keyLen
+        outstr += outlist[col][row]
+
+    return outstr
+
+
 s = input("Type your message: ")
 key = input("Type your key: ")
+s = encrypt(s, key)
+print("Encrypted String is: ", s)
 
-# Encryption
-keyLen = len(key)
-padLen = len(s) % keyLen
-
-# pads the message s so with spaces so that it can be encrypted
-if padLen > 0:
-    for i in range(0, keyLen - padLen):
-        s = s + " "
-
-sList = []
-ind = 0
-while ind < len(s):
-    sList.append(list(s[ind: ind + keyLen]))
-    ind = ind + keyLen
-keyList = list(key)
-keyList.sort()
-
-newS = ""
-for i in range(0, len(keyList)):
-    for x in range(0, len(sList)):
-        newS = newS + sList[x][keyList.index(key[i: i + 1])]
-print("Encrypted String is: ", newS)
-
-# Decryption
-outlist = []
-for i in range(0, keyLen):
-    outlist.append([])
-
-numrows = math.ceil(len(newS) / keyLen)
-
-fullcols = len(newS) % keyLen
-if fullcols == 0:
-    fullcols = keyLen
-lenslice = 0
-begslice = 0
-
-for index in range(0, keyLen):
-    keyletter = key[index]
-    keyindex = keyList.index(keyletter)
-
-    if keyindex >= fullcols:
-        lenslice = numrows - 1
-    else:
-        lenslice = numrows
-
-    colslice = newS[begslice: begslice + lenslice]
-    begslice += lenslice
-
-    outlist[keyindex] = list(colslice)
-
-outstr = ""
-
-for index in range(0, len(newS)):
-    row = index // keyLen
-    col = index % keyLen
-    outstr += outlist[col][row]
+outstr = decrypt(s, key)
 
 print("Decrypted string is: ", outstr)

cb8354846ee12d80ea10d28879fd44b7ff7b0152
Robert Boutillier <36680316+rbout@users.noreply.github.com>
2018-09-27 11:25:12 -0400

    Bug fixes

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import math
 
-def encrypt(str, key):
+
+def encrypt(userS, key):
     # Encryption
     keyLen = len(key)
-    padLen = len(s) % keyLen
+    padLen = len(userS) % keyLen
 
     # pads the message s so with spaces so that it can be encrypted
     if padLen > 0:
         for i in range(0, keyLen - padLen):
-            str = str + " "
+            userS = userS + " "
 
     sList = []
     ind = 0
-    while ind < len(s):
-        sList.append(list(str[ind: ind + keyLen]))
+    while ind < len(userS):
+        sList.append(list(userS[ind: ind + keyLen]))
         ind = ind + keyLen
     keyList = list(key)
     keyList.sort()
@@ -26,18 +27,19 @@ def encrypt(str, key):
     return newS
 
 
-def decrypt(str, key):
+def decrypt(newUserS, key):
     # Decryption
     keyLen = len(key)
     keyList = list(key)
+    keyList.sort()
 
     outlist = []
     for i in range(0, keyLen):
         outlist.append([])
 
-    numrows = math.ceil(len(str) / keyLen)
+    numrows = math.ceil(len(newUserS) / keyLen)
 
-    fullcols = len(s) % keyLen
+    fullcols = len(newUserS) % keyLen
     if fullcols == 0:
         fullcols = keyLen
 
@@ -53,14 +55,14 @@ def decrypt(str, key):
         else:
             lenslice = numrows
 
-        colslice = str[begslice: begslice + lenslice]
+        colslice = newUserS[begslice: begslice + lenslice]
         begslice += lenslice
 
         outlist[keyindex] = list(colslice)
 
     outstr = ""
 
-    for index in range(0, len(str)):
+    for index in range(0, len(newUserS)):
         row = index // keyLen
         col = index % keyLen
         outstr += outlist[col][row]
@@ -69,10 +71,10 @@ def decrypt(str, key):
 
 
 s = input("Type your message: ")
-key = input("Type your key: ")
-s = encrypt(s, key)
+k = input("Type your key: ")
+s = encrypt(s, k)
 print("Encrypted String is: ", s)
 
-outstr = decrypt(s, key)
+outstr = decrypt(s, k)
 
 print("Decrypted string is: ", outstr)
